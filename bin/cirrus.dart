cbb6002c7208e5e8d753e7704f5b9a67b5013d55
Cesar Parra <cesar.parra@gmail.com>
2025-07-17 05:57:18 -0400

    Refactor CLI structure and add initial RunCommand implementation

--- bin/cirrus.dart
+++ bin/cirrus.dart
@@ -1,23 +1,8 @@
 import 'package:args/args.dart';
+import 'package:args/command_runner.dart';
+import 'src/run.dart';
 
-const String version = '0.0.1';
-
-ArgParser buildParser() {
-  return ArgParser()
-    ..addFlag(
-      'help',
-      abbr: 'h',
-      negatable: false,
-      help: 'Print this usage information.',
-    )
-    ..addFlag(
-      'verbose',
-      abbr: 'v',
-      negatable: false,
-      help: 'Show additional command output.',
-    )
-    ..addFlag('version', negatable: false, help: 'Print the tool version.');
-}
+const String version = '0.0.3';
 
 void printUsage(ArgParser argParser) {
   print('Usage: dart cirrus.dart <flags> [arguments]');
@@ -25,33 +10,10 @@ void printUsage(ArgParser argParser) {
 }
 
 void main(List<String> arguments) {
-  final ArgParser argParser = buildParser();
-  try {
-    final ArgResults results = argParser.parse(arguments);
-    bool verbose = false;
-
-    // Process the parsed arguments.
-    if (results.flag('help')) {
-      printUsage(argParser);
-      return;
-    }
-    if (results.flag('version')) {
-      print('cirrus version: $version');
-      return;
-    }
-    if (results.flag('verbose')) {
-      verbose = true;
-    }
-
-    // Act on the arguments provided.
-    print('Positional arguments: ${results.rest}');
-    if (verbose) {
-      print('[VERBOSE] All arguments: ${results.arguments}');
-    }
-  } on FormatException catch (e) {
-    // Print usage information if an invalid argument was provided.
-    print(e.message);
-    print('');
-    printUsage(argParser);
-  }
+  CommandRunner(
+      "cirrus",
+      "A lean command-line interface tool for Salesforce development automation.",
+    )
+    ..addCommand(RunCommand())
+    ..run(arguments);
 }
