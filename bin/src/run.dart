81dfc6fa5ad3c3045d73736dda5334023479eaee
Cesar Parra <cesar.parra@gmail.com>
2025-07-18 07:55:24 -0400

    Wrapping the sf org scratch create command

--- bin/src/run.dart
+++ bin/src/run.dart
@@ -1,4 +1,5 @@
 import 'package:args/command_runner.dart';
+import 'package:cli_script/cli_script.dart' as cli;
 
 class RunCommand extends Command {
   @override
@@ -8,11 +9,56 @@ class RunCommand extends Command {
   String get description => 'Runs a standalone command';
 
   RunCommand() {
-    argParser.addFlag('wepa', negatable: false, help: 'Hello');
+    addSubcommand(CreateScratchCommand());
+  }
+}
+
+// Default run commands
+
+class CreateScratchCommand extends Command {
+  @override
+  final name = 'create_scratch';
+
+  @override
+  String get description => 'Creates a scratch org.';
+
+  CreateScratchCommand() {
+    argParser
+      ..addFlag(
+        'alias',
+        abbr: 'a',
+        negatable: false,
+        help: 'The alias to assign to the scratch org.',
+      )
+      ..addFlag(
+        'set-default',
+        abbr: 'd',
+        negatable: false,
+        help: 'Sets the created scratch org as the default one',
+      )
+      ..addOption(
+        'definition-file',
+        abbr: 'f',
+        mandatory: true,
+        help: 'The definition file location',
+      )
+      ..addOption(
+        'target-dev-hub',
+        abbr: 'v',
+        help: 'The definition file location',
+      )
+      ..addOption(
+        'duration-days',
+        abbr: 'y',
+        help: 'Number of days before the org expires.',
+      );
   }
 
   @override
-  void run() {
-    print('Run wepa!');
+  Future<void> run() async {
+    // print('about to run create_scratch');
+    // print(argResults!.option("file"));
+    // print('going to run ${argResults?.arguments ?? 'no args'}');
+    await cli.run('sf org scratch create', args: argResults?.arguments);
   }
 }
