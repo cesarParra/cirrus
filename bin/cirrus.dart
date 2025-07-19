447862733396687eadb5814c616a3b8677cbc41a
Cesar Parra <cesar.parra@gmail.com>
2025-07-19 08:52:28 -0400

    Create Scratch Org command implementation

--- bin/cirrus.dart
+++ bin/cirrus.dart
@@ -1,10 +1,9 @@
 import 'package:args/command_runner.dart';
+import 'package:chalkdart/chalkdart.dart';
 import 'package:fpdart/fpdart.dart';
 import 'package:toml/toml.dart';
 import 'src/run.dart';
 import 'dart:io';
-
-const String version = '0.0.3';
 
 Future<void> main(List<String> arguments) async {
   final runner =
@@ -12,11 +11,14 @@ Future<void> main(List<String> arguments) async {
         () => TomlDocument.loadSync("cirrus.toml").toMap(),
         (_, _) =>
             "Was not able to load the cirrus.toml file. Make sure it exists",
-      ).map(
-        (document) => CommandRunner(
-          "cirrus",
-          "A lean command-line interface tool for Salesforce development automation.",
-        )..addCommand(RunCommand(document)),
+      ).flatMap(
+        (document) => Either.tryCatch(
+          () => CommandRunner(
+            "cirrus",
+            "A lean command-line interface tool for Salesforce development automation.",
+          )..addCommand(RunCommand(document)),
+          (error, _) => 'Unexpected error: $error',
+        ),
       );
 
   switch (runner) {
@@ -24,13 +26,15 @@ Future<void> main(List<String> arguments) async {
       try {
         await value.run(arguments);
       } on UsageException catch (e) {
-        stderr.writeln(e.message);
+        stderr.writeln(chalk.red(e.message));
         print('');
         value.printUsage();
+      } catch (e) {
+        stderr.write(chalk.red('$e'));
       }
 
     case Left(:final value):
-      stderr.writeln(value);
+      stderr.writeln(chalk.red(value));
       print("");
   }
 }
