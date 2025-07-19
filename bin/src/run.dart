ddeab891c78b86a54d83043147a82ba067b6331f
Cesar Parra <cesar.parra@gmail.com>
2025-07-19 08:52:28 -0400

    Create Scratch Org command implementation

--- bin/src/run.dart
+++ bin/src/run.dart
@@ -1,5 +1,7 @@
 import 'package:args/command_runner.dart';
+import 'package:chalkdart/chalkdart.dart';
 import 'package:cli_script/cli_script.dart' as cli;
+import 'package:fpdart/fpdart.dart';
 
 class RunCommand extends Command {
   @override
@@ -9,53 +11,93 @@ class RunCommand extends Command {
   String get description => 'Runs a standalone command';
 
   RunCommand(Map<String, dynamic> config) {
-    addSubcommand(CreateScratchCommand());
+    // Parse the config file to pass the necessary information
+    // each individual command needs
+
+    final orgDefinitions = switch (config) {
+      {'orgs': List<dynamic> orgs} =>
+        orgs.map(ScratchOrgDefinition.parse).toList(),
+      _ => <ScratchOrgDefinition>[],
+    };
+
+    addSubcommand(CreateScratchCommand(orgDefinitions));
   }
 }
 
 // Default run commands
 
+// TODO: Alias support
+// TODO: Set default support
+// TODO: Target devhub support
+class ScratchOrgDefinition {
+  final String name;
+  final String definitionFile;
+  final int? duration;
+
+  ScratchOrgDefinition(this.name, this.definitionFile, [this.duration]);
+
+  static ScratchOrgDefinition parse(dynamic def) {
+    return switch (def) {
+      {"name": String name, "definitionFile": String definitionFile} =>
+        ScratchOrgDefinition(name, definitionFile, def['duration']),
+      _ => throw 'Could not parse scratch org definition.',
+    };
+  }
+}
+
 class CreateScratchCommand extends Command {
+  final List<ScratchOrgDefinition> definitions;
+
   @override
   final name = 'create_scratch';
 
   @override
   String get description => 'Creates a scratch org.';
 
-  CreateScratchCommand() {
-    argParser
-      ..addFlag(
-        'alias',
-        abbr: 'a',
-        negatable: false,
-        help: 'The alias to assign to the scratch org.',
-      )
-      ..addFlag(
-        'set-default',
-        abbr: 'd',
-        negatable: false,
-        help: 'Sets the created scratch org as the default one',
-      )
-      ..addOption(
-        'definition-file',
-        abbr: 'f',
-        mandatory: true,
-        help: 'The definition file location',
-      )
-      ..addOption(
-        'target-dev-hub',
-        abbr: 'v',
-        help: 'The definition file location',
-      )
-      ..addOption(
-        'duration-days',
-        abbr: 'y',
-        help: 'Number of days before the org expires.',
-      );
+  CreateScratchCommand(this.definitions) {
+    argParser.addOption(
+      'name',
+      abbr: 'n',
+      mandatory: true,
+      help: 'The name of the scratch org definition to create',
+    );
   }
 
   @override
   Future<void> run() async {
-    //await cli.run('sf org scratch create', args: argResults?.arguments);
+    final orgName = argResults?.option('name') ?? '';
+    final orgDefinition = definitions.firstWhereOrOption(
+      (def) => def.name == orgName,
+    );
+
+    switch (orgDefinition) {
+      case Some(:final value):
+        final command = build(value);
+        print(chalk.green('Running: $command'));
+        await cli.run(command);
+      case None():
+        throw 'The org "$orgName" is not defined in the cirrus.toml file.';
+    }
+  }
+
+  String build(ScratchOrgDefinition orgDefinition) {
+    final root =
+        'sf org scratch create --definition-file=${orgDefinition.definitionFile}';
+
+    if (orgDefinition.duration == null) {
+      return root;
+    }
+
+    return '$root --duration-days=${orgDefinition.duration}';
+  }
+}
+
+extension IterableExtestons<T> on Iterable<T> {
+  Option<T> firstWhereOrOption(Function(T) f) {
+    for (final current in this) {
+      if (f(current)) return Some(current);
+    }
+
+    return None();
   }
 }
