442f81856c20e8e95cc1b4c39f33c66d829e6473
Cesar Parra <cesar.parra@gmail.com>
2025-07-19 07:41:59 -0400

    Enhance command capabilities and update dependencies in TODO and pubspec files

--- bin/cirrus.dart
+++ bin/cirrus.dart
@@ -1,20 +1,36 @@
 import 'package:args/command_runner.dart';
+import 'package:fpdart/fpdart.dart';
+import 'package:toml/toml.dart';
 import 'src/run.dart';
 import 'dart:io';
 
 const String version = '0.0.3';
 
 Future<void> main(List<String> arguments) async {
-  final runner = CommandRunner(
-    "cirrus",
-    "A lean command-line interface tool for Salesforce development automation.",
-  )..addCommand(RunCommand());
+  final runner =
+      Either.tryCatch(
+        () => TomlDocument.loadSync("cirrus.toml").toMap(),
+        (_, _) =>
+            "Was not able to load the cirrus.toml file. Make sure it exists",
+      ).map(
+        (document) => CommandRunner(
+          "cirrus",
+          "A lean command-line interface tool for Salesforce development automation.",
+        )..addCommand(RunCommand(document)),
+      );
 
-  try {
-    await runner.run(arguments);
-  } on UsageException catch (e) {
-    stderr.writeln(e.message);
-    print('');
-    runner.printUsage();
+  switch (runner) {
+    case Right(:final value):
+      try {
+        await value.run(arguments);
+      } on UsageException catch (e) {
+        stderr.writeln(e.message);
+        print('');
+        value.printUsage();
+      }
+
+    case Left(:final value):
+      stderr.writeln(value);
+      print("");
   }
 }
